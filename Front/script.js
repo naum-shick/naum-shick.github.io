c83a1586ac52d3779fca5869dc5a667f99079c41
Naum Shick <naumka@gmail.com>
2024-05-23 21:49:11 +0300

    author msg

--- Front/script.js
+++ Front/script.js
@@ -216,7 +216,7 @@ async function saveSignature(shift) {
 function about() {
   blurt(
     "Signature",
-    "А вот это программу так еще не доделали Яна Шик и Наум Шик;  " +
+    "А вот это программу продолжают модифицировать Яна Шик и Наум Шик;  " +
       `innerWidth = ${window.innerWidth}`,
     "info"
   );
